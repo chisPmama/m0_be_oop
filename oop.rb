52488ef6208844933a34fa1214c41ea114d829af
Anthea Yur <acyur6@gmail.com>
2023-08-08 13:04:10 -0500

    Add answers and completed segment

--- oop.rb
+++ oop.rb
@@ -3,6 +3,22 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+attr_reader :name, :color, :input_string
+    def initialize(name)
+        @name = name
+        @color = "silver"
+    end
+   
+    def say(input_string)
+        p "*~* I am #{@name} the #{@color} unicorn and I just wanted to say #{input_string}! *~*"
+    end
+end
+
+unicorn1 = Unicorn.new("Jasmine")
+p unicorn1
+unicorn1.say("I love you")
+p unicorn1
 
 
 #  Write a class called Vampire
@@ -11,6 +27,26 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+
+    def drink
+        @thirsty = false
+    end
+end
+
+vampire1 = Vampire.new("Dracula")
+p vampire1
+
+vampire2 = Vampire.new("Count Dude","wolf")
+p vampire2
+
+vampire2.drink
+p vampire2
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -19,6 +55,28 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    attr_reader :name, :rider, :color
+    def initialize(name, rider, color)
+      @name = name
+      @rider = rider
+      @color = color
+      @is_hungry = true
+    end
+    def eat(number)
+      if number >= 4
+        @is_hungry = false
+        "Dragon is no longer hungry!"
+      else
+        "Dragon is still hungry!"
+      end
+    end
+  end
+
+  dragon1 = Dragon.new("Charzard", "Broc", "red")
+  p dragon1
+dragon1.eat(8)
+p dragon1
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +86,45 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+
+    def initialize(name, disposition,age = 0)
+        @name = name
+        @disposition = disposition
+        @age = age
+        @is_adult = false
+        @is_old = false
+        @has_ring = false
+
+        if @name == "Frodo"
+            @has_ring = true
+        end
+    end
+
+    def celebrate_birthday(age)
+        new_age = age +=1
+        @age = new_age
+    end
+
+    def adult_old_verification(age)
+        @age = age
+        if age >= 33 && age >= 101
+            @is_adult = true
+            @is_old = true
+        elsif age >= 33
+            @is_adult = true
+        end
+end
+
+
+hobbit1 = Hobbit.new("Frodo", "naive")
+p hobbit1
+
+hobbit2 = Hobbit.new("Orlando Bloom", "simp")
+p hobbit2
+hobbit2.adult_old_verification(80)
+p hobbit2
+hobbit2.celebrate_birthday(90)
+p hobbit2
+end
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,8 @@
 # Self-Evaluation
 
 - This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+    I feel good about it! It felt fluid and I'm glad we took the time to review how to assess errors. I did have one weird requirement to add another "end" when I was printing out my code and testing it, which I can't quite find where the missing end belongs to. But other than that, it feels fluid and comprehensive!
 - How is your workflow going, in terms of taking breaks, staying focused, etc.?
+    I don't have a lot of time to take breaks and come back so I do just sit and get it all done within a time block, but that will change when school kicks into full-time because I'll have more spare time to actually take breaks and do something recreationally in between.
 - What did you learn from this exercise that you want to remember as you begin Mod 1?
+    I learned a lot using the +=1 as well as remembering the difference from @age and age, I would like to start practicing maybe more parameter and argument names to try to better differentiate the two? Not sure if that is an advantage or a suggested method.
